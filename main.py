2cdee02090111e3af05a52a6e50464847b80047b
magnesyljuasen <msylju@gmail.com>
2023-08-14 15:42:20 +0200

    fungerer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+pd.options.mode.chained_assignment = None
 import numpy as np
 from requests_oauthlib import OAuth2Session
 from oauthlib.oauth2 import BackendApplicationClient
@@ -94,10 +95,13 @@ class EnergyAnalysis:
         self.SOLAR_PANELS = 'solceller'
         self.AIR_SOURCE_HEAT_PUMP = 'luft_luft_varmepumpe'
         self.DISTRICT_HEATING = 'fjernvarme'
+        #-- fra matrikkel
         self.OBJECT_ID = 'objektid'
         self.BUILDING_STANDARD = 'byggstandard_profet'
         self.BUILDING_TYPE = 'byggtype_profet'
-        self.AREA = 'areal'
+        self.BUILDING_AREA = 'areal'
+        self.BUILT_AREA = 'bebygd_areal'
+        #-- fra matrikkel
         self.TEMPERATURE_ARRAY = '_utetemperatur'
         self.THERMAL_DEMAND = '_termisk_energibehov'
         self.ELECTRIC_DEMAND = '_elspesifikt_energibehov'
@@ -121,14 +125,15 @@ class EnergyAnalysis:
         n_values = int((percentage / 100) * number_of_rows)
         random_indices = random.sample(range(number_of_rows), n_values)
         random_values = [fill_value for _ in range(n_values)]
+        df = df.sort_values(self.OBJECT_ID).reset_index(drop=True)
         df.loc[random_indices, column_name] = random_values
         return df
     
     def _add_values_randomly_thermal(self, df, prosentfordeling, innfyllingsverdi=True):
         p1, p2, p3 = (
-            prosentfordeling["grunnvarme"],
-            prosentfordeling["fjernvarme"],
-            prosentfordeling["varmepumpe"],
+            prosentfordeling[self.GSHP],
+            prosentfordeling[self.DISTRICT_HEATING],
+            prosentfordeling[self.AIR_SOURCE_HEAT_PUMP],
         )
         c1, c2, c3 = (
             self.GSHP,
@@ -172,6 +177,7 @@ class EnergyAnalysis:
         for supply_technology in [self.GSHP, self.SOLAR_PANELS, self.AIR_SOURCE_HEAT_PUMP, self.DISTRICT_HEATING]:
             df[supply_technology] = 0
         # go through energy_scenario dict
+        modified_df_list = []
         for building_type in energy_scenario.keys():
             df_building_type = df.loc[df[self.BUILDING_TYPE] == building_type]
             for supply_type in energy_scenario[building_type].split("_"):
@@ -191,16 +197,16 @@ class EnergyAnalysis:
                 if "O" in supply_type:
                     percentage_oppgradert = self.__string_percentage(string = supply_type) 
             percentages = {
-                "grunnvarme" : percentage_grunnvarme,
-                "fjernvarme" : percentage_fjernvarme,
-                "varmepumpe" : percentage_varmepumpe,
+                self.GSHP : percentage_grunnvarme,
+                self.DISTRICT_HEATING : percentage_fjernvarme,
+                self.AIR_SOURCE_HEAT_PUMP : percentage_varmepumpe,
             }
             modified_df = self._add_values_randomly_thermal(df = df_building_type, prosentfordeling=percentages)
             modified_df = self.__add_values_randomly(df = modified_df, column_name = self.SOLAR_PANELS, percentage = percentage_solceller, fill_value = fill_value )
-            break
-            
-        # based on condition in dataframe
-        #df[self.GSHP] = np.where(df[self.BUILDING_TYPE] == 'B', 1, 0)
+            modified_df_list.append(modified_df)
+        #-- Merge alle dataframes i områdeliste
+        modified_df = pd.concat(modified_df_list).reset_index(drop=True)
+        modified_df = modified_df.sort_values(self.OBJECT_ID).reset_index(drop=True)
         return modified_df
 
     def modify_scenario(self, df, energy_scenario = "a"):
@@ -278,14 +284,14 @@ class EnergyAnalysis:
 
     def profet_calculation_simplified(self, row):
         thermal_demand_series = self.PROFET_DATA[f"{row[self.BUILDING_TYPE]}_{row[self.BUILDING_STANDARD]}_THERMAL"]
-        thermal_demand = row[self.AREA] * np.array(thermal_demand_series)
+        thermal_demand = row[self.BUILDING_AREA] * np.array(thermal_demand_series)
         electric_demand_series = self.PROFET_DATA[f"{row[self.BUILDING_TYPE]}_{row[self.BUILDING_STANDARD]}_ELECTRIC"]
-        electric_demand = row[self.AREA] * np.array(electric_demand_series)
+        electric_demand = row[self.BUILDING_AREA] * np.array(electric_demand_series)
         return thermal_demand, electric_demand
 
     def profet_calculation(self, row):
         # profet API logic
-        thermal_demand, electric_demand = self.__profet_api(building_standard = row[self.BUILDING_STANDARD], building_type = row[self.BUILDING_TYPE], area = row[self.AREA], temperature_array = row[self.TEMPERATURE_ARRAY])
+        thermal_demand, electric_demand = self.__profet_api(building_standard = row[self.BUILDING_STANDARD], building_type = row[self.BUILDING_TYPE], area = row[self.BUILDING_AREA], temperature_array = row[self.TEMPERATURE_ARRAY])
         return thermal_demand, electric_demand
 
     def __dekningsgrad_calculation(self, dekningsgrad, timeserie):
@@ -341,8 +347,9 @@ class EnergyAnalysis:
 
     def solcelle_calculation(self, row):
         solceller = 0
+        area = row[self.BUILT_AREA]
         if row[self.SOLAR_PANELS] == 1:
-            solceller = row[self.AREA] * self.SOLARPANEL_DATA[self.SOLARPANEL_BUILDINGS[row[self.BUILDING_TYPE]]].to_numpy()
+            solceller = area * self.SOLARPANEL_DATA[self.SOLARPANEL_BUILDINGS[row[self.BUILDING_TYPE]]].to_numpy()
         return -solceller
 
     def compile_data(self, row):
@@ -350,24 +357,42 @@ class EnergyAnalysis:
         electric_balance = row[self.ELECTRIC_DEMAND] + row[self.COMPRESSOR] + row[self.PEAK] + row[self.SOLAR_PANELS_PRODUCED]
         total_balance = thermal_balance + electric_balance
         return total_balance, round(np.sum(total_balance),-2), round(np.max(total_balance),0)
+    
+    def chunkify(self, df, chunk_size):
+        list_df = [df[i:i+chunk_size] for i in range(0,df.shape[0],chunk_size)]
+        return list_df
 
-    def run_simulation(self, df, scenario_name, preprocessing = True):
-        # profet / demand
-        if preprocessing == True:
-            df[self.THERMAL_DEMAND], df[self.ELECTRIC_DEMAND] = zip(*df.swifter.apply(self.profet_calculation_simplified, axis=1))
-        else:
-            df[self.THERMAL_DEMAND], df[self.ELECTRIC_DEMAND] = zip(*df.swifter.apply(self.profet_calculation, axis=1))
-        # supply
-        df[self.COMPRESSOR], df[self.FROM_SOURCE], df[self.PEAK] = zip(*df.swifter.apply(self.varmepumpe_calculation, axis=1))
-        df[self.DISTRICT_HEATING_PRODUCED] = df.swifter.apply(self.fjernvarme_calculation, axis=1)
-        df[self.SOLAR_PANELS_PRODUCED] = df.swifter.apply(self.solcelle_calculation, axis=1)
-        # conclusion
-        df[f'_nettutveksling_energi_liste'], df[f'_nettutveksling_energi'], df[f'_nettutveksling_effekt'] = zip(*df.swifter.apply(self.compile_data, axis=1))
-        df.to_csv(f"output/{scenario_name}_unfiltered.csv")
-        # cleanup lists
-        df.drop([self.TEMPERATURE_ARRAY, self.THERMAL_DEMAND, self.ELECTRIC_DEMAND, self.COMPRESSOR, self.FROM_SOURCE, self.PEAK, self.DISTRICT_HEATING_PRODUCED, self.SOLAR_PANELS_PRODUCED, f'_nettutveksling_energi_liste'], axis=1, inplace=True)
-        df.to_csv(f"output/{scenario_name}_filtered.csv")
-        return df
+    def run_simulation(self, df, scenario_name, preprocessing = True, chunk_size = 10):
+        def __merge_dataframe_list(df_chunked_list):
+            df_results = pd.concat(df_chunked_list).reset_index(drop=True)
+            df_results = df_results.sort_values(self.OBJECT_ID).reset_index(drop=True)
+            df_results.to_csv(f"output/{scenario_name}_filtered.csv")
+            return df_results
+            
+        df_chunked_list = []
+        chunked = self.chunkify(df = df, chunk_size = chunk_size)
+        for index, df_chunked in enumerate(chunked):
+            # profet / demand
+            if preprocessing == True:
+                df_chunked[self.THERMAL_DEMAND], df_chunked[self.ELECTRIC_DEMAND] = zip(*df_chunked.swifter.apply(self.profet_calculation_simplified, axis=1))
+            else:
+                df_chunked[self.THERMAL_DEMAND], df_chunked[self.ELECTRIC_DEMAND] = zip(*df_chunked.swifter.apply(self.profet_calculation, axis=1))
+            # supply
+            df_chunked[self.COMPRESSOR], df_chunked[self.FROM_SOURCE], df_chunked[self.PEAK] = zip(*df_chunked.swifter.apply(self.varmepumpe_calculation, axis=1))
+            df_chunked[self.DISTRICT_HEATING_PRODUCED] = df_chunked.swifter.apply(self.fjernvarme_calculation, axis=1)
+            df_chunked[self.SOLAR_PANELS_PRODUCED] = df_chunked.swifter.apply(self.solcelle_calculation, axis=1)
+            # conclusion
+            df_chunked[f'_nettutveksling_energi_liste'], df_chunked[f'_nettutveksling_energi'], df_chunked[f'_nettutveksling_effekt'] = zip(*df_chunked.swifter.apply(self.compile_data, axis=1))
+            df_chunked.to_csv(f"output/{scenario_name}_unfiltered.csv")
+            # cleanup lists
+            df_chunked.drop([self.TEMPERATURE_ARRAY, self.THERMAL_DEMAND, self.ELECTRIC_DEMAND, self.COMPRESSOR, self.FROM_SOURCE, self.PEAK, self.DISTRICT_HEATING_PRODUCED, self.SOLAR_PANELS_PRODUCED, f'_nettutveksling_energi_liste'], axis=1, inplace=True)
+            # append to
+            df_chunked_list.append(df_chunked)
+            # write more often, for every 5 chunk
+            if (index % 5) == 0:
+                __merge_dataframe_list(df_chunked_list)
+        df_results = __merge_dataframe_list(df_chunked_list)
+        return df_results
 
     def to_arcgis(self, df):
         pass
@@ -387,8 +412,8 @@ class EnergyAnalysis:
         #profet_data = preprocess_profet_data(df = table)
         # -- simulation 1
         energy_dict = ({
-            "A" : "S90_O50_F60_G40_V50",
-            "B" : "F50_G20_V20_S40",
+            "A" : "S30_O50_F60_G40_V50",
+            "B" : "F80_G20_V20_S40",
             "C" : "F20_V60_S20",
             "D" : "F10_V60_S20",
             "E" : "F50_V40_S20",
@@ -401,15 +426,15 @@ class EnergyAnalysis:
             "L" : "F20_V60_S20",
         })
         table = self.create_scenario(df = table, energy_scenario = energy_dict)
-        print(table)
-        #table = self.add_temperature_series(df = table, temperature_series = "default")
-        #table = self.run_simulation(df = table, scenario_name = "S1")
+        table = self.add_temperature_series(df = table, temperature_series = "default")
+        table = self.run_simulation(df = table, scenario_name = "S1")
         # -- simulation 2
-        #table = self.modify_scenario(df = table)
-        #table = self.add_temperature_series(df = table, temperature_series = "default")
-        #table = self.run_simulation(df = table, scenario_name = "S2")
+        table = self.modify_scenario(df = table)
+        table = self.add_temperature_series(df = table, temperature_series = "default")
+        table = self.run_simulation(df = table, scenario_name = "S2")
         # -- simulation 3
         #to_arcgis(df = table)
+        print(table)
 
 # mangler på valg av scenario_konsept
 if __name__ == '__main__':
